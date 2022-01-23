a410ed294cf7de7b7437d2b560dd02d49b8cdd42
madeng <madeng5555@gmail.com>
2022-01-23 12:28:56 +0100

    aoc21: Restructure run.sh for easier debugging compilation problem

--- 2021/run.sh
+++ 2021/run.sh
@@ -1,41 +1,64 @@
-#!/bin/sh
+#!/bin/sh -e
 #DEBUG=1
 MOD_FOLDER='build'
 
-if [ -z $DEBUG ]; then
-	exec 3>&1 4>&2 >.run.log 2>&1
-fi
+redirect() {
+	if [ -z $DEBUG ]; then
+		exec 3>&1 4>&2 >.run.log 2>&1
+	fi
+}
+
+# Return 0 if there was redirection, 1 otherwise
+stop_redirect() {
+	if [ -z $DEBUG ]; then
+		# Restore the redirection before executing the python script
+		exec >&3 2>&4
+		return 0
+	fi
+	return 1
+}
+
+compile() {
+	MAIN_PYX=$1/.main.pyx
+	cp $1/main.py $MAIN_PYX
+
+	sed -i '1 s/^/# cython: language_level=3\n/' $MAIN_PYX
+
+	[ -d $MOD_FOLDER ] || mkdir $MOD_FOLDER
+
+	cat >.setup.py <<-EOF
+	from setuptools import setup, Extension
+	setup(name='$1', ext_modules=[Extension('$MOD_FOLDER.c$1', sources=['$MAIN_PYX'])])
+	EOF
+
+	. venv/bin/activate
+	python .setup.py build_ext --inplace || return 1
+
+	RUN_FILE='.run_'$1'.py'
+	cat >$RUN_FILE <<-EOF
+	import $MOD_FOLDER.c$1
+	EOF
+
+	RUN_FILE_PATH=$(realpath $RUN_FILE)
+}
 
 die() {
 	echo $@ >&2
 	exit 1
 }
 
-[ $# -ge 1 ] || die "Missing folder to execute!"
 [ -f $1/main.py ] || die 'There is no main.py in folder: '$1
 
-cp $1/main.py $1/.main.pyx
+redirect
 
-[ -d $MOD_FOLDER ] || mkdir $MOD_FOLDER
+compile $1 || {
+	echo stopping redirect..
+	stop_redirect && python .setup.py build_ext --inplace
+}
 
-cat >.setup.py <<EOF
-from setuptools import setup, Extension
-setup(name='$1', ext_modules=[Extension('$MOD_FOLDER.c$1', sources=['$1/.main.pyx'])])
-EOF
+stop_redirect
 
-. venv/bin/activate
-python .setup.py build_ext --inplace
-
-RUN_FILE='.run_'$1'.py'
-cat >$RUN_FILE <<EOF
-import $MOD_FOLDER.$1
-EOF
-
-FULL_RUN_FILE_PATH=$(realpath $RUN_FILE)
-cd $1
-if [ -z $DEBUG ]; then
-	# Restore the redirection before executing the python script
-	exec >&3 2>&4
-fi
-python $FULL_RUN_FILE_PATH
+# Pass all the other arguments of the script to the actual python script
+shift
+python $RUN_FILE_PATH $@
 
