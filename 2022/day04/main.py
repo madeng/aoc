df925331a7ab99606ee7ca9c00ca561a3c2cc445
madeng <madeng5555@gmail.com>
2022-12-03 11:59:21 +0100

    aoc22: prepare day 4

---
+++ 2022/day04/main.py
@@ -0,0 +1,21 @@
+import sys
+
+FILE = sys.argv[1] if len(sys.argv) > 1 else 'in'
+
+P1 = 0
+P2 = 0
+
+for I, DLINE in enumerate(open(FILE)):
+	L = DLINE.strip()
+	# part 1
+
+
+
+
+
+
+
+
+
+print("Part 1 ==>  {}\n".format(P1))
+print("Part 2 ==>  {}\n".format(P2))
