55e6a6ea5bfab018288d36da6d00c18e4d1dd05d
madeng <madeng5555@gmail.com>
2022-12-04 13:23:08 +0100

    aoc22: solve day4

--- 2022/day04/main.py
+++ 2022/day04/main.py
@@ -7,15 +7,17 @@ P2 = 0
 
 for I, DLINE in enumerate(open(FILE)):
 	L = DLINE.strip()
-	# part 1
+	E1,E2 = L.split(",")
+	X,Y = [int(i) for i in E1.split("-")]
+	A,B = [int(i) for i in E2.split("-")]
 
+	if (X >= A and Y <= B) or (A >= X and B <= Y):
+		P1 += 1
+		P2 += 1
+	elif (X <= B and Y >= A) or (A <= Y and B >= X):
+		P2 += 1
 
-
-
-
-
-
-
-
-print("Part 1 ==>  {}\n".format(P1))
-print("Part 2 ==>  {}\n".format(P2))
+# 603 too high
+# 584 is right
+print("({}) Part 1 ==>  {}\n".format(FILE, P1))
+print("({}) Part 2 ==>  {}\n".format(FILE, P2))
