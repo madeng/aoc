c9810a98a9ec531e1ba3576dbb6c494ff53eaf71
madeng <madeng5555@gmail.com>
2022-12-08 09:40:10 +0100

    aoc22: prepare day09

---
+++ 2022/day09/part1.py
@@ -0,0 +1,20 @@
+import sys
+
+strip = lambda x : x.strip('\n')
+LINES = list(map(strip, open(sys.argv[1] if len(sys.argv) > 1 else 'in').readlines()))
+
+LX = len(LINES)
+LY = len(LINES[0])
+ANS = 0
+
+
+
+
+
+
+for L in LINES:
+	pass
+
+
+ANS = 0
+print("ANSWER ==>  {}\n".format(ANS))
