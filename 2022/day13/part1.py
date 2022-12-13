8183aad0c5bb876c17e03bd6a5fc3c5acc4176df
madeng <madeng5555@gmail.com>
2022-12-12 21:56:50 -0500

    aoc22: prepare day 11 and day 13

--- 2022/day13/part1.py
+++ 2022/day13/part1.py
@@ -2,11 +2,12 @@ import sys
 
 strip = lambda x : x.strip('\n')
 LINES = list(map(strip, open(sys.argv[1] if len(sys.argv) > 1 else 'in').readlines()))
-
-for L in LINES:
-	pass
-
-
+LX = len(LINES)
+LY = len(LINES[0])
 ANS = 0
+
+
+
 #
-print("ANSWER ==>  {}\n".format(ANS))
+if ANS != 0:
+	print("ANSWER ==>  {}\n".format(ANS))
